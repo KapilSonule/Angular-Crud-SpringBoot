c6884fd38780d105194fc6997dea77f862c89188
Kapil <kapsonule@gmail.com>
2020-08-29 18:19:01 +0530

    Second Commit

---
+++ src/app/model/employee.model.ts
@@ -0,0 +1,9 @@
+import { commodity } from 'src/app/commodity-list/commodity';
+import { weightdesc } from './weightdesc';
+export class Employee {
+  id: number;
+  name: string;
+  email: string;
+  phone: number;
+  department: string;
+}
